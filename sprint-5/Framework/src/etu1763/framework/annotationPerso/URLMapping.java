194a665826bc5986c54ac5f7a9436d8091e48bc5
FaAndriacode <119219904+FaAndriacode@users.noreply.github.com>
2023-07-04 11:07:23 +0300

    Sprint-10
    Sprint-10

--- sprint-5/Framework/src/etu1763/framework/annotationPerso/URLMapping.java
+++
@@ -1,9 +0,0 @@
-package etu1763.framework.annotationPerso;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface URLMapping {
-    String valeur() default "/";
-}
